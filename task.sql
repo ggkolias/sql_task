ef53ca9569c9b11459f053bfccd813299cd57fae
Georgios Gkolias <giorgosgolias@gmail.com>
2018-07-24 15:07:41 +0200

    Update task.sql

--- task.sql
+++ task.sql
@@ -1,6 +1,9 @@
-/*after adding the table with the data that are provided in a doc file via email,
-we were ready to access the created table*/
+/*after adding the table with the data that are provided to me in a doc file via email,
+i was able to access the created table and find a query which shows the number 
+of rows where jsonbtest.to_jsonb's b and c fields are both 0*/
 
 SELECT count(jsontest.to_jsonb)
 FROM public.jsontest
 WHERE to_jsonb @> '{"b": 0, "c": 0}';
+
+/*the result is: 6666 rows*/
